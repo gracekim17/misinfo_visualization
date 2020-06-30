b4f8947d8f840d9dca992c3474dfe0c959bc45e7
Grace Kim <kimgrace2000@gmail.com>
2020-06-30 12:31:32 -0700

    Updates

---
+++ precache-manifest.a3a118f4aedd090edbace4c2ab9ae461.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86612dee8fa2130d9e7e3f8e6a89c3af",
+    "url": "/misinfo_visualization/index.html"
+  },
+  {
+    "revision": "283fecc566cf32e1290c",
+    "url": "/misinfo_visualization/static/css/main.a996307e.chunk.css"
+  },
+  {
+    "revision": "e8560b73bfa14ba5f6d0",
+    "url": "/misinfo_visualization/static/js/2.35791a5c.chunk.js"
+  },
+  {
+    "revision": "436f23c45f716d0ee89b73f3096ad986",
+    "url": "/misinfo_visualization/static/js/2.35791a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283fecc566cf32e1290c",
+    "url": "/misinfo_visualization/static/js/main.7022286b.chunk.js"
+  },
+  {
+    "revision": "5b7b7dec75c5a8787499",
+    "url": "/misinfo_visualization/static/js/runtime-main.9fb4b352.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/misinfo_visualization/static/media/logo.5d5d9eef.svg"
+  }
+]);
